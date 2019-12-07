a45ef230a34e98456fbcbb0d059f882529f47cf6
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-12-07 01:29:17 +0100

    persist token

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router";
-import { useMutation } from "urql";
-import uuidv4 from "uuid/v4";
 import { send } from "ionicons/icons";
-import createPersistedState from "use-persisted-state";
 
 import {
   IonItem,
@@ -14,44 +11,85 @@ import {
   IonHeader,
   IonToolbar,
   IonTitle,
-  IonButtons,
-  IonBackButton,
-  IonPage,
-  IonTextarea
+  IonPage
 } from "@ionic/react";
 
 import ButtonFooter from "../components/ButtonFooter";
-import GraphQLFetch from "../components/GraphQLFetch";
-import getPerson from "../queries/getPerson";
-import createTask from "../mutations/createTask";
+import { setToken } from "../token";
 
-const useLoginState = createPersistedState("login");
+const login = ({ username, password }) =>
+  fetch("http://127.0.0.1:1337/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username,
+      password
+    })
+  })
+    .then(res => res.json())
+    .then(json => {
+      if (json.error) {
+        throw new Error(json.error);
+      }
+      return json;
+    });
 
 const LoginBox = () => {
-  const [login, setLogin] = useLoginState("");
-  let password = "";
-  const status = "";
-  const onSubmit = () => {};
+  let loginInput;
+  let passwordInput;
+  const [status, setStatus] = useState("idle");
+  const history = useHistory();
+  const [showMessage, setShowMessage] = useState(false);
+  const onSubmit = () => {
+    setStatus("submitting");
+    login({
+      username: loginInput.value,
+      password: passwordInput.value
+    })
+      .then(r => {
+        if (r.success) {
+          setToken(r.user.token);
+          history.push("/tasks");
+          return;
+        }
+      })
+      .catch(e => {
+        setStatus("error");
+        setShowMessage("Erreur: " + e.message);
+        setTimeout(() => {
+          setStatus("idle");
+          setShowMessage(false);
+        }, 2000);
+      });
+  };
   return (
-    <React.Fragment>
-      <IonItem>
-        <IonLabel>Login</IonLabel>
-        <IonInput value={login}></IonInput>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Mot de passe</IonLabel>
-        <IonInput
-          onKeyUp={e => (password = e.target.value)}
-          type="password"
-        ></IonInput>
-      </IonItem>
+    <div style={{ paddingTop: 100, width: 300, margin: "0 auto" }}>
+      <div>
+        <IonItem>
+          <IonLabel>Email</IonLabel>
+          <IonInput ref={node => (loginInput = node)}></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Mot de passe</IonLabel>
+          <IonInput
+            ref={node => (passwordInput = node)}
+            type="password"
+          ></IonInput>
+        </IonItem>
+      </div>
+      <div style={{ margin: "20px 0", textAlign: "center" }}>
+        {showMessage}&nbsp;
+      </div>
       <ButtonFooter
+        style={{ marginTop: 50 }}
         disabled={status === "submitting"}
         text="se connecter"
         icon={send}
         onClick={onSubmit}
       />
-    </React.Fragment>
+    </div>
   );
 };
 
